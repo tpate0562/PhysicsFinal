252ada2576aa6b5ff6625c48f65e7755f41034c2
tpate0562 <56415907+tpate0562@users.noreply.github.com>
2024-04-30 11:00:34 -0700

    Update Lab 14 Mars trip template.py

--- Lab 14 Mars trip template.py
+++ Lab 14 Mars trip template.py
@@ -1,4 +1,4 @@
-from vpython import sphere, vector, color, rate, scene, attach_trail, arrow, label, cos, sin, pi, mag, norm
+from vpython import sphere, vector, color, rate, scene, attach_trail, arrow, label, cos, sin, pi, mag, norm, hat
 
 scene.width = 630
 scene.height = 600
@@ -17,29 +17,29 @@ scene.userzoom = True
 # "mag(a)"   is a scalar that is the magnitude of the vector "a"
 
 # Constants in metric units
-marsdiameter = 6.78e6      # mars diameter
+jupiterdiameter = 6.78e6      # jupiter diameter
 G = 6.67384e-11              # universal gravitational constant
 AU = 1.496e11                # Astronomical unit (avg. dist from Sun to Earth--for length scale purposes)
 msun = 1.989e30              # mass of Sol (sun)
 mearth = 5.97219e24              # mass of Earth
-mmars = 6.4185e23              # mass of Mars
-mrocket = 1e4              # mass of rocket
+mjupiter = 1.898e27           # mass of jupiter
+mrocket = 815              # mass of rocket
 
 # *** Control panel (all quantities in metric units)
 # *** To simplify the problem, set the z-component of 
 # *** the initial positions and velocities below to zero
 
 dt = 2 * 3600                          # time step
-marsrocketdist = 500 * marsdiameter    # distance from mars considered a success
-riearth = vector(9.062260787481210e10, -1.202386386794547e11 ,0)                   # initial position of mars
-viearth = vector(2.327335600544946e4, 1.782202031258401e4,0)
-rimars = vector(1.837669141112841e11, -9.170458197260064e10,0)                 # initial position of earth
-vimars = vector(1.178684789960390e4, 2.371545627374556e4,0)                     # initial velocity of earth
-                    # initial velocity of mars
-speedirocket = 5e3                         #initial speed of rocket
-angleirocket =    6.881                        # launch angle of rocket (relative to +x axis)
+jupiterrocketdist = 500 * jupiterdiameter    # distance from jupiter considered a success
+riearth = vector(1.447214054379151E+11, -4.423991320885015E+10, -1.062619063954428E+07)                   # initial position of jupiter
+viearth = vector(8.148892366116470E+03, 2.840375889886787E+04, 1.639043726347822E+00)
+rijupiter = vector(1.055355055205870E+11, 7.546173434489038E+11, -5.471696100017726E+09)                 # initial position of earth
+vijupiter = vector(-1.309430711064983E+04, 2.413277002306963E+03, 2.833049104713701E+02)                     # initial velocity of earth
+                    # initial velocity of jupiter
+virocket =vector(1.621225177117242E+04, 4.048629375968122E+04, 8.055511810827731E+02)             #initial speed of rocket
+rirocket =  vector(1.447289329190070E+11, -4.424029803569365E+10, -1.103055189367570E+07)          # launch angle of rocket (relative to +x axis)
 
-virocket = speedirocket*vector(cos(angleirocket*pi/180),sin(angleirocket*pi/180),0)
+
 
 
 # set up scene and objects
@@ -51,13 +51,13 @@ earth=sphere(pos=riearth, radius=0.05*AU, color=color.blue)
 earthtrail=attach_trail(earth, radius=0.2*earth.radius, trail_type="points", interval=2, retain=1000)
 earth.vel = viearth
 
-mars=sphere(pos=rimars, radius=0.0001*AU, color=color.red)
-marstrail=attach_trail(mars, radius=0.2*mars.radius, trail_type="points", interval=2, retain=1000)
-mars.vel = vimars
+jupiter=sphere(pos=rijupiter, radius=0.08*AU, color=color.red)
+jupitertrail=attach_trail(jupiter, radius=0.2*jupiter.radius, trail_type="points", interval=2, retain=1000)
+jupiter.vel = vijupiter
 
-rocket=sphere(pos=earth.pos, radius=0.0001*AU, color=color.orange)
+rocket=sphere(pos=rirocket, radius=0.05*AU, color=color.orange)
 rockettrail=attach_trail(rocket, radius=0.2*rocket.radius, trail_type="points", interval=2, retain=1000)
-rocket.vel = earth.vel + virocket
+rocket.vel = virocket
 scene.camera.follow(rocket)
 
 # draw an arrow to show direction of initial velocity of rocket
@@ -65,23 +65,23 @@ rocketarrow1 = arrow(pos=earth.pos, axis=(sun.radius*2)*norm(virocket), color=co
 
 
 #set the scene
-scene.range=1.3*mag(mars.pos)
+scene.range=1.3*mag(jupiter.pos)
 
 #create display for timing information
 tstr="Time: {:.0f} days".format(0)
-tlabel=label(pos=vector(0,1.2*mag(mars.pos),0), text=tstr)
+tlabel=label(pos=vector(0,1.2*mag(jupiter.pos),0), text=tstr)
 
 launchstr="Starting Date: 07302020."             # *** replace XXXXXXXX with your launch date
 
-launchlabel=label(pos=vector(0,-1.2*mag(mars.pos),0), text=launchstr)
+launchlabel=label(pos=vector(0,-1.2*mag(jupiter.pos),0), text=launchstr)
 
 
 t=0
 
 # *** add a comparison to the while statement below (inside the parentheses)
-# *** so that the program will run until the rocket is within "marsrocketdist" of Mars
-while mag(mars.pos - rocket.pos) > marsrocketdist*0.0005:
-    rate(30)  # Controls the speed of the simulation
+# *** so that the program will run until the rocket is within "jupiterrocketdist" of jupiter
+while 1:
+    rate(200)  # Controls the speed of the simulation
 
     # Gravitational force calculation
     def gravitational_force(m1, m2, r1, r2):
@@ -98,16 +98,16 @@ while mag(mars.pos - rocket.pos) > marsrocketdist*0.0005:
     earth.vel += f_earth_sun / mearth * dt
     earth.pos += earth.vel * dt
 
-    # Mars's motion
-    f_mars_sun = gravitational_force(mmars, msun, mars.pos, sun.pos)
-    mars.vel += f_mars_sun / mmars * dt
-    mars.pos += mars.vel * dt
+    # jupiter's motion
+    f_jupiter_sun = gravitational_force(mjupiter, msun, jupiter.pos, sun.pos)
+    jupiter.vel += f_jupiter_sun / mjupiter * dt
+    jupiter.pos += jupiter.vel * dt
 
     # Rocket's motion
     f_rocket_sun = gravitational_force(mrocket, msun, rocket.pos, sun.pos)
     f_rocket_earth = gravitational_force(mrocket, mearth, rocket.pos, earth.pos)
-    f_rocket_mars = gravitational_force(mrocket, mmars, rocket.pos, mars.pos)
-    total_force = f_rocket_sun + f_rocket_earth + f_rocket_mars
+    f_rocket_jupiter = gravitational_force(mrocket, mjupiter, rocket.pos, jupiter.pos)
+    total_force = f_rocket_sun + f_rocket_earth + f_rocket_jupiter
     rocket.vel += total_force / mrocket * dt
     rocket.pos += rocket.vel * dt
 
@@ -115,3 +115,4 @@ while mag(mars.pos - rocket.pos) > marsrocketdist*0.0005:
     t += dt
     tstr = "Time: {:.0f} days".format(t / (24 * 3600))
     tlabel.text = tstr
+    print(mag(rocket.pos-jupiter.pos))
